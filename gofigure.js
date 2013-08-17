20c88aec4eb57a0096d49d36ca2eded09710bffa
Erlend Oftedal <erlend@oftedal.no>
2013-08-17 20:47:29 +0200

    Line and arrow

--- gofigure.js
+++ gofigure.js
@@ -1,33 +1,35 @@
-/*global $:false, console:false, Raphael:false */
+/*global $:false, Raphael:false */
 
 var gofigure = function() {
     var box = function(x, y, w, h) {
         w -= 12.875*2;
         h -= 12.876*2;
         x += 12.875;
-        return { pathString: "M" + x + "," + y + 
-            "h" + w + "c7.109,0,12.875,5.765,12.875,12.876 " + 
-            "v" + h + "c0,7.109,-5.765,12.875,-12.875,12.876 " + 
-            "h-" + w + "c-7.109,0,-12.875,-5.765,-12.875,-12.876 " + 
-            "v-" + h + " c0,-7.109,5.765,-12.875,12.875,-12.876", 
+        return { pathString: "M" + x + "," + y +
+            "h" + w + "c7.109,0,12.875,5.765,12.875,12.876 " +
+            "v" + h + "c0,7.109,-5.765,12.875,-12.875,12.876 " +
+            "h-" + w + "c-7.109,0,-12.875,-5.765,-12.875,-12.876 " +
+            "v-" + h + " c0,-7.109,5.765,-12.875,12.875,-12.876",
             join: join };
     };
+
     var round = function(num, places) {
         return Math.round(num * Math.pow(10, places)) / Math.pow(10, places);
-    }
-    var arrow = function(x1, y1, x2, y2, options) {
-        var heads = (options || {}).heads || "both";
+    };
+
+    var line = function(x1, y1, x2, y2, options) {
+        var heads = (options || {}).arrowheads || "none";
         var len = (options || {}).len || 10;
         var ang = Math.atan((x1-x2)/(y1-y2));
-        var begin = (heads == "end") ? "" : ("M" + x1 + "," + y1 + 
+        var begin = (heads != "begin" && heads != "both") ? "" : ("M" + x1 + "," + y1 +
             "l" + -round(Math.sin(ang - Math.PI/4)*len, 3) + "," + round(Math.cos(ang - Math.PI / 4)*len, 3) +
-            "M" + x1 + "," + y1 + 
+            "M" + x1 + "," + y1 +
             "l" + -round(Math.sin(ang + Math.PI/4)*len, 3) + "," + round(Math.cos(ang + Math.PI / 4)*len, 3));
-        var end = (heads == "begin") ? "" : ("M" + x2 + "," + y2 + 
+        var end = (heads != "end" && heads != "both") ? "" : ("M" + x2 + "," + y2 +
             "l" + round(Math.sin(ang - Math.PI/4)*len, 3) + "," + round(Math.cos(ang - Math.PI / 4)*len, 3) +
-            "M" + x2 + "," + y2 + 
+            "M" + x2 + "," + y2 +
             "l" + round(Math.sin(ang + Math.PI/4)*len, 3) + "," + round(Math.cos(ang + Math.PI / 4)*len, 3));
-        return  { pathString: 
+        return  { pathString:
             begin +
             "M" + x1 + "," + y1 +
             "l" + (x2 - x1) + "," + (y2 - y1) +
@@ -47,7 +49,7 @@ var gofigure = function() {
             minLeft = minLeft < n ? minLeft : n;
         }
         return drawtext(canvas, x - (maxRight - minLeft)/2.0, y, text);
-    }
+    };
 
 
     var drawtext = function(canvas, x, y, text) {
@@ -75,27 +77,27 @@ var gofigure = function() {
 
     function setupLine(canvas, group, part) {
         var line = canvas.path(part.path.pathString).attr({
-            stroke: "#000",
-            'stroke-width': 0
+            "stroke": "#000",
+            "stroke-width": 0
         });
         var identifier = group + "-animate-" + gofigure_lineid++;
-        line.node.setAttribute('class', identifier);
-        return { line : line, identifier : identifier, options : part.options };        
+        line.node.setAttribute("class", identifier);
+        return { line : line, identifier : identifier, options : part.options };
     }
 
     function animate(canvas, lineObj, callback) {
         var line = lineObj.line;
         var length = line.getTotalLength();
         var current = null;
-        $('path.' + lineObj.identifier + '[fill*="none"]').animate({
-            'to': 1
+        $("path." + lineObj.identifier + '[fill*="none"]').animate({
+            "to": 1
         }, {
             duration: lineObj.options.duration || 1000,
             step: function(pos, fx) {
                 var offset = length * fx.pos;
                 var subpath = line.getSubpath(0, offset);
                 var subline = canvas.path(subpath).attr({
-                    'stroke-width': lineObj.options.strokeWidth || 3,
+                    "stroke-width": lineObj.options.strokeWidth || 3,
                     stroke: "#000",
                     fill: lineObj.options.fill || "none"
                 });
@@ -105,7 +107,7 @@ var gofigure = function() {
                 current = subline;
             },
             done: function() {
-                callback && callback();
+                if (callback) callback();
             }
         });
     }
@@ -123,25 +125,29 @@ var gofigure = function() {
         }
         function run(lines, i) {
             if (i >= lines.length) return;
-            animate(canvas, lines[i], function() { run(lines, i + 1)});
+            animate(canvas, lines[i], function() { run(lines, i + 1); });
         }
         run(lines, 0);
     }
 
     function createStep(canvas, group) {
         var step = { parts: [] };
-        step.box = function(x, y, width, height, options) { 
-            return prepare(step, box(x, y, width, height), {}, options); 
+        step.box = function(x, y, width, height, options) {
+            return prepare(step, box(x, y, width, height), {}, options);
         };
-        step.centeredText = function(x, y, text, options) { 
-            return prepare(step, drawcenteredtext(canvas, x, y, text), { strokeWidth: -1, fill: "#000" }, options); 
+        step.centeredText = function(x, y, text, options) {
+            return prepare(step, drawcenteredtext(canvas, x, y, text), { strokeWidth: -1, fill: "#000" }, options);
         };
-        step.arrow = function(xfrom, yfrom, xto, yto, options) { 
-            return prepare(step, arrow(xfrom, yfrom, xto, yto, options), {}, options); 
+        step.line = function(xfrom, yfrom, xto, yto, options) {
+            return prepare(step, line(xfrom, yfrom, xto, yto, options), {}, options);
         };
-        step.animate = function() { 
+        step.arrow = function(xfrom, yfrom, xto, yto, options) {
+            options2 = $.extend({ arrowheads : "both"}, options);
+            return prepare(step, line(xfrom, yfrom, xto, yto, options2), {}, options);
+        };
+        step.animate = function() {
             doAnimate(canvas, group, step.parts);
-        }
+        };
         return step;
     }
 
@@ -149,7 +155,7 @@ var gofigure = function() {
     function create(containerId, width, height) {
         var canvas = Raphael(containerId, width, height);
         var group = "group" + gofigureid++;
-        return fig = { 
+        return {
             bindClick : function(steps) {
                 var i = 0;
                 $("#" + containerId + " svg").click(function() {
@@ -160,13 +166,15 @@ var gofigure = function() {
                 });
             },
             animate: function() {},
-            box: function(x, y, width, height, options) { return createStep(canvas, group).box(x, y, width, height, options) },
-            centeredText: function(x, y, text, options) { return createStep(canvas, group).centeredText(x, y, text, options) },
-            arrow: function(xfrom, yfrom, xto, yto, options) { return createStep(canvas, group).arrow(xfrom, yfrom, xto, yto, options) },
-        }
+            box: function(x, y, width, height, options) { return createStep(canvas, group).box(x, y, width, height, options); },
+            centeredText: function(x, y, text, options) { return createStep(canvas, group).centeredText(x, y, text, options); },
+            line: function(xfrom, yfrom, xto, yto, options) { return createStep(canvas, group).line(xfrom, yfrom, xto, yto, options); },
+            arrow: function(xfrom, yfrom, xto, yto, options) { return createStep(canvas, group).arrow(xfrom, yfrom, xto, yto, options); },
+
+        };
     }
 
-    return { 
+    return {
         create: create
-	}; 
+	};
 }();
