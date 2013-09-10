50a6f16b0b46abd8591d8cb5913690628fb423fe
Erlend Oftedal <erlend@oftedal.no>
2013-09-11 00:16:19 +0200

    Adding first beta of reveal plugin

---
+++ slide-plugins/reveal-js/Readme.md
@@ -0,0 +1,26 @@
+How to
+------
+
+1. Copy the plugin/gofigure folder into the plugin directory in your reveal presentation
+2. Add the plugin under `Render.initialize`, `dependencies`:
+	{ src: 'plugin/gofigure/plugin.js',  condition: function() { return true; } }
+3. Add a div with an id to a slide (the id 'figure' is used below)
+4. Create the steps:
+		<script>
+
+		window.addEventListener('load', function() {
+		  setTimeout(function() {
+			var figure = gofigure.create('figure', '780', '450');
+		    var opts = {arrowheads: "end", duration:500 };
+		    gofigureplugin.addStep('figure', figure.arrow(10, 10, 40, 40, opts));
+		    gofigureplugin.addStep('figure', figure.arrow(50, 10, 50, 40, opts));
+		    gofigureplugin.addStep('figure', figure.arrow(90, 10, 60, 40, opts));
+		    gofigureplugin.addStep('figure', figure.arrow(10, 50, 40, 50, opts));
+		    gofigureplugin.addStep('figure', figure.arrow(90, 50, 60, 50, opts));
+		  }, 1000);
+		}, false);
+
+		</script>
+
+
+The setTimeout is a bit ugly, but seems to be needed to make sure all libraries are loaded.
---
+++ slide-plugins/reveal-js/plugin/gofigure/plugin.js
@@ -0,0 +1,37 @@
+function addScript(src) {
+	var s = document.createElement("script");
+	s.src = src;
+	document.body.appendChild(s);
+}
+
+addScript("plugin/gofigure/lib/raphael-min.js");
+addScript("plugin/gofigure/lib/cufon.js");
+addScript("plugin/gofigure/lib/Vegur.js");
+addScript("plugin/gofigure/lib/jquery.min.js");
+addScript("plugin/gofigure/gofigure.js");
+
+var gofigureplugin = {
+	steps : {},
+	isListening : false,
+	addStep : function(id, animatable) {
+		this.steps[id] = this.steps[id] || [];
+		this.steps[id].push(animatable);
+		var s = document.createElement("span");
+		s.className = "fragment ";
+		s.gofigureid = id;
+		document.getElementById(id).appendChild(s);
+		if (!this.isListening) {
+			this.isListening = true;
+			var those = this.steps;
+			Reveal.addEventListener( 'fragmentshown', function(event) {
+				if (event.fragment.gofigureid && those[event.fragment.gofigureid].length > 0) {
+					those[event.fragment.gofigureid].reverse();
+					those[event.fragment.gofigureid].pop().animate();
+					those[event.fragment.gofigureid].reverse();
+					event.fragment.remove();
+				}
+ 			});
+		}
+	}
+};
+
